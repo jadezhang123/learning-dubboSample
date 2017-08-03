226b5758e5fd37c08d3a31fb5e1849d4a5968388
ZhangJunwei <604110740@qq.com>
2017-08-03 23:03:12 +0800

    spring-session集成redis共享session

---
+++ learning-dubboSample-domain/src/main/java/own/jadezhang/learning/dubboSample/common/Constant.java
@@ -0,0 +1,11 @@
+package own.jadezhang.learning.dubboSample.common;
+
+/**
+ * Created by Zhang Junwei on 2017/8/3.
+ */
+public class Constant {
+    /**
+     * 登录用户session key
+     */
+    public static final String LOGIN_USER = "login_user";
+}
---
+++ learning-dubboSample-web/src/main/java/own/jadezhang/learning/dubboSample/controller/user/UserController.java
@@ -0,0 +1,51 @@
+package own.jadezhang.learning.dubboSample.controller.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import own.jadezhang.learning.dubboSample.api.dto.AuthUerDto;
+import own.jadezhang.learning.dubboSample.api.rpc.IUserService4Rpc;
+import own.jadezhang.learning.dubboSample.common.Constant;
+import own.jadezhang.learning.dubboSample.common.ResultDto;
+import own.jadezhang.learning.dubboSample.domain.auth.AuthUser;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Zhang Junwei on 2017/7/22.
+ */
+@Controller
+@RequestMapping("/dubboSample/user")
+public class UserController {
+    @Autowired
+    private IUserService4Rpc userService4Rpc;
+
+    @ResponseBody
+    @RequestMapping(value = "/findOne")
+    public AuthUerDto findOne() {
+        AuthUerDto user = userService4Rpc.findUser("082ba75a22a648a680e3ea7982e7ecdb");
+        return user;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/login")
+    public ResultDto login(HttpServletRequest request, String userName, String password) {
+        AuthUser user = new AuthUser();
+        user.setName(userName);
+        user.setPassword(password);
+        request.getSession().setAttribute(Constant.LOGIN_USER, user);
+        return new ResultDto(true, "登录成功");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/index")
+    public ResultDto index(HttpServletRequest request) {
+        AuthUser user = (AuthUser) request.getSession().getAttribute(Constant.LOGIN_USER);
+        if (user != null) {
+            return new ResultDto(true, user);
+        }
+        return new ResultDto(false, "当前用户未登录");
+    }
+
+}
